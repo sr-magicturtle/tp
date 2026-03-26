b86649d90cce62f20c6cdd7a22f08a990f25a2da
legendgirlie47 <hongtianyu05@gmail.com>
2026-03-26 18:35:02 +0800

    Add tests for new person fields for storage

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -1,6 +1,7 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.storage.JsonAdaptedPerson.MISSING_FIELD_MESSAGE_FORMAT;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.BENSON;
@@ -14,8 +15,11 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.FollowUpDate;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+
 
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
@@ -23,6 +27,9 @@ public class JsonAdaptedPersonTest {
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_TAG = "#friend";
+    private static final String INVALID_FOLLOW_UP_DATE = "invalid-date";
+    private static final String INVALID_NOTE = "   ";
+    private static final String INVALID_CIRCLE = "enemy";
 
     private static final String VALID_NAME = BENSON.getName().toString();
     private static final String VALID_PHONE = BENSON.getPhone().toString();
@@ -31,6 +38,10 @@ public class JsonAdaptedPersonTest {
     private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.getTags().stream()
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList());
+    private static final String VALID_FOLLOW_UP_DATE = "2025-12-01";
+    private static final String VALID_NOTE = "hello world";
+    private static final String VALID_CIRCLE = "client";
+
 
     @Test
     public void toModelType_validPersonDetails_returnsPerson() throws Exception {
@@ -116,4 +127,85 @@ public class JsonAdaptedPersonTest {
         assertThrows(IllegalValueException.class, person::toModelType);
     }
 
+    @Test
+    public void toModelType_validFollowUpDate_success() throws Exception {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, VALID_TAGS,
+                        VALID_FOLLOW_UP_DATE, null, null);
+
+        Person modelPerson = person.toModelType();
+        assertEquals(VALID_FOLLOW_UP_DATE,
+                modelPerson.getFollowUpDate().get().toString());
+    }
+
+    @Test
+    public void toModelType_invalidFollowUpDate_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, VALID_TAGS,
+                        INVALID_FOLLOW_UP_DATE, null, null);
+
+        assertThrows(IllegalValueException.class,
+                FollowUpDate.MESSAGE_CONSTRAINTS,
+                person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullFollowUpDate_success() throws Exception {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, VALID_TAGS,
+                        null, null, null);
+
+        Person modelPerson = person.toModelType();
+        assertTrue(modelPerson.getFollowUpDate().isEmpty());
+    }
+
+    @Test
+    public void toModelType_validCircle_success() throws Exception {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, VALID_TAGS,
+                        null, null, VALID_CIRCLE);
+
+        Person modelPerson = person.toModelType();
+        assertEquals(VALID_CIRCLE, modelPerson.getCircle().get());
+    }
+
+    @Test
+    public void toModelType_invalidCircle_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, VALID_TAGS,
+                        null, null, INVALID_CIRCLE);
+
+        assertThrows(IllegalValueException.class,
+                "Circle must be one of: client, prospect, friend.",
+                person::toModelType);
+    }
+
+    @Test
+    public void toModelType_validNote_success() throws Exception {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, VALID_TAGS,
+                        null, VALID_NOTE, null);
+
+        Person modelPerson = person.toModelType();
+        assertEquals(VALID_NOTE,
+                modelPerson.getNotes().get().toString());
+    }
+
+    @Test
+    public void toModelType_invalidNote_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, VALID_TAGS,
+                        null, INVALID_NOTE, null);
+
+        assertThrows(IllegalArgumentException.class,
+                person::toModelType);
+    }
 }
+
