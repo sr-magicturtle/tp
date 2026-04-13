0b307115904022891e6d11e2ddafa074ec9da153
zhangshuhan05 <zshuhan512@gmail.com>
2026-04-13 16:01:02 +0800

    Fix checkstyletest

--- src/test/java/seedu/address/logic/parser/CircleAddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CircleAddCommandParserTest.java
@@ -3,9 +3,9 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_DUPLICATE_FIELDS;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CIRCLE;
-import static seedu.address.testutil.TypicalCircles.FRIENDS;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalCircles.FRIENDS;
 
 import org.junit.jupiter.api.Test;
 
